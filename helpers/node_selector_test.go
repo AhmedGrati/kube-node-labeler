6af665639b14e031fbefcbdb359dc5f585aabe3c
ahmed <ahmedgrati1999@gmail.com>
2022-06-25 00:48:19 +0000

    finish testing nodes matching with node selector terms

--- helpers/node_selector_test.go
+++ helpers/node_selector_test.go
@@ -8,7 +8,20 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+)
+
+var (
+	InvalidNsm = []corev1.NodeSelectorRequirement{
+		{
+			Key:      "os",
+			Values:   []string{"LINUX"},
+			Operator: "PIWPIW",
+		},
+	}
+	EmptyNsm = []corev1.NodeSelectorRequirement{}
+	Node     = getBasicNode()
 )
 
 func TestValidNodeSelectorRequirementsAsSelector(t *testing.T) {
@@ -26,36 +39,42 @@ func TestValidNodeSelectorRequirementsAsSelector(t *testing.T) {
 		corev1.NodeSelectorOpLt,
 	}
 	for _, op := range inOperators {
-		validNsm := []corev1.NodeSelectorRequirement{
-			{
-				Key:      "os",
-				Operator: op,
-				Values:   []string{"LINUX"},
-			},
-		}
-		selector, err := NodeSelectorRequirementsAsSelector(validNsm)
-		assertLabelsAndRequirementsEquality(t, selector, validNsm, err, true)
+		t.Run(string(op), func(t *testing.T) {
+			validNsm := []corev1.NodeSelectorRequirement{
+				{
+					Key:      "os",
+					Operator: op,
+					Values:   []string{"LINUX"},
+				},
+			}
+			selector, err := NodeSelectorRequirementsAsSelector(validNsm)
+			assertLabelsAndRequirementsEquality(t, selector, validNsm, err, true)
+		})
 	}
 	for _, op := range existOperatos {
-		validNsm := []corev1.NodeSelectorRequirement{
-			{
-				Key:      "ip-address",
-				Operator: op,
-			},
-		}
-		selector, err := NodeSelectorRequirementsAsSelector(validNsm)
-		assertLabelsAndRequirementsEquality(t, selector, validNsm, err, false)
+		t.Run(string(op), func(t *testing.T) {
+			validNsm := []corev1.NodeSelectorRequirement{
+				{
+					Key:      "ip-address",
+					Operator: op,
+				},
+			}
+			selector, err := NodeSelectorRequirementsAsSelector(validNsm)
+			assertLabelsAndRequirementsEquality(t, selector, validNsm, err, false)
+		})
 	}
 	for _, op := range comparatifOperators {
-		validNsm := []corev1.NodeSelectorRequirement{
-			{
-				Key:      "number-of-years",
-				Operator: op,
-				Values:   []string{"2"},
-			},
-		}
-		selector, err := NodeSelectorRequirementsAsSelector(validNsm)
-		assertLabelsAndRequirementsEquality(t, selector, validNsm, err, true)
+		t.Run(string(op), func(t *testing.T) {
+			validNsm := []corev1.NodeSelectorRequirement{
+				{
+					Key:      "number-of-years",
+					Operator: op,
+					Values:   []string{"2"},
+				},
+			}
+			selector, err := NodeSelectorRequirementsAsSelector(validNsm)
+			assertLabelsAndRequirementsEquality(t, selector, validNsm, err, true)
+		})
 	}
 }
 
@@ -75,22 +94,109 @@ func assertLabelsAndRequirementsEquality(t *testing.T, selector labels.Selector,
 }
 
 func TestInvalidNodeSelectorRequirementsAsSelector(t *testing.T) {
-	invalidNsm := []corev1.NodeSelectorRequirement{
-		{
-			Key:      "os",
-			Values:   []string{"LINUX"},
-			Operator: "PIWPIW",
-		},
-	}
-	labels, err := NodeSelectorRequirementsAsSelector(invalidNsm)
+	labels, err := NodeSelectorRequirementsAsSelector(InvalidNsm)
 	assert.Error(t, err)
 	assert.Nil(t, labels)
 }
 
 func TestEmptyNodeSelectorRequirementsAsSelector(t *testing.T) {
-	emptyNsm := []corev1.NodeSelectorRequirement{}
-	res, err := NodeSelectorRequirementsAsSelector(emptyNsm)
+	res, err := NodeSelectorRequirementsAsSelector(EmptyNsm)
 
 	assert.NoError(t, err)
 	assert.Equal(t, res, labels.Nothing())
 }
+
+func getBasicNode() *corev1.Node {
+	return &corev1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "basic-node",
+			Labels: map[string]string{
+				"os":              "linux",
+				"number-of-years": "2",
+				"ip-address":      "127.0.0.1",
+			},
+		},
+		Spec: corev1.NodeSpec{
+			Taints: []corev1.Taint{
+				{
+					Key:    "key1",
+					Value:  "value1",
+					Effect: corev1.TaintEffectNoExecute,
+				},
+			},
+		},
+	}
+}
+
+func TestNodesMatchingWithInvalidSelector(t *testing.T) {
+	invalidNodeSelectorTerms := []corev1.NodeSelectorTerm{
+		{
+			MatchExpressions: InvalidNsm,
+		},
+	}
+	isMatch := NodeMatchesNodeSelectorTerms(Node, invalidNodeSelectorTerms)
+	assert.False(t, isMatch)
+}
+func TestNodesMatchingWithEmptyTerms(t *testing.T) {
+	invalidNodeSelectorTerms := []corev1.NodeSelectorTerm{}
+	isMatch := NodeMatchesNodeSelectorTerms(Node, invalidNodeSelectorTerms)
+	assert.False(t, isMatch)
+}
+
+func TestNodesMatchingWithValidTerms(t *testing.T) {
+	matchingRequirements := []corev1.NodeSelectorTerm{
+		{
+			MatchExpressions: []corev1.NodeSelectorRequirement{
+				{
+					Key:      "os",
+					Values:   []string{"linux"},
+					Operator: corev1.NodeSelectorOpIn,
+				},
+			},
+		},
+		{
+			MatchExpressions: []corev1.NodeSelectorRequirement{
+				{
+					Key:      "ip-address",
+					Values:   []string{"172.0.10.1"},
+					Operator: corev1.NodeSelectorOpIn,
+				},
+			},
+		},
+	}
+	isMatch := NodeMatchesNodeSelectorTerms(Node, matchingRequirements)
+	assert.True(t, isMatch)
+
+	nonMatchingRequirements := []corev1.NodeSelectorTerm{
+		{
+			MatchExpressions: []corev1.NodeSelectorRequirement{
+				{
+					Key:      "os",
+					Values:   []string{"linux"},
+					Operator: corev1.NodeSelectorOpIn,
+				},
+				{
+					Key:      "ip-address",
+					Values:   []string{"172.0.10.1"},
+					Operator: corev1.NodeSelectorOpIn,
+				},
+			},
+		},
+		{
+			MatchExpressions: []corev1.NodeSelectorRequirement{
+				{
+					Key:      "ip-address",
+					Values:   []string{"172.0.10.1"},
+					Operator: corev1.NodeSelectorOpIn,
+				},
+			},
+		},
+	}
+
+	isMatch = NodeMatchesNodeSelectorTerms(Node, nonMatchingRequirements)
+	assert.False(t, isMatch)
+}
+
+// func TestNodeListsMerging(t *testing.T) {
+
+// }
